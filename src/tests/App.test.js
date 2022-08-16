375d02f7850c87027ce000fba1fe5f896e2935e8
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-08-16 18:59:46 -0300

    Requisito 1, teste componenente App realizado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente APP', () => {
+  test('É exibido na tela um link com o texto Home', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: /home/i });
+    expect(linkHome).toBeInTheDocument();
+  });
+
+  test('É exibido na tela um link com o texto About', () => {
+    renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: /about/i });
+    expect(linkAbout).toBeInTheDocument();
+  });
+  test('É exibido na tela um link com o texto Favorite Pokémons', () => {
+    renderWithRouter(<App />);
+    const linkFavorite = screen.getByRole('link', { name: /favorite/i });
+    expect(linkFavorite).toBeInTheDocument();
+  });
+});
